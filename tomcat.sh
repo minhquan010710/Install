8059e02d21495588e225dc5d16b692174b903aaf
minhquan010710 <37204913+minhquan010710@users.noreply.github.com>
2020-02-01 13:51:50 +0700

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-sudo yum install java-1.8.0 -y
 cd /
-mkdir tomcat
+sudo yum install java-1.8.0 -y
+
+sudo mkdir tomcat
 cd tomcat
-wget http://mirror.downloadvn.com/apache/tomcat/tomcat-9/v9.0.30/bin/apache-tomcat-9.0.30.zip
-unzip apache-tomcat-9.0.30.zip
-cd /apache-tomcat-9.0.30/bin
-chmod +x * 
-./catalina.sh start 
+sudo wget http://mirror.downloadvn.com/apache/tomcat/tomcat-9/v9.0.30/bin/apache-tomcat-9.0.30.zip
+sudo unzip apache-tomcat-9.0.30.zip
+cd apache-tomcat-9.0.30/bin
+sudo chmod +x * 
+sudo ./catalina.sh start 
